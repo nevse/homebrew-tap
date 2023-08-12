55bb3657c92aa87ca7db78535b7e7fd81332e121
nevse <nov.eugen@gmail.com>
2023-08-13 00:00:19 +0400

    fix installer

--- Casks/conva.rb
+++ Casks/conva.rb
@@ -2,14 +2,14 @@ cask "conva" do
   version "0.1.8"
 
   on_arm do
-    sha256 "201e4d90a029a2182fb70dc51b8474463ba125f47b99a6cac06f18326bb72e46"
-
-    url "https://github.com/nevse/conva/releases/download/v#{version}/conva-darwin-amd64.tar.gz"
-  end
-  on_intel do
     sha256 "d51906383525bac23c61fb73dd292f6ff9149581df09437ca490cb6fc328f305"
 
     url "https://github.com/nevse/conva/releases/download/v#{version}/conva-darwin-arm64.tar.gz"
+  end
+  on_intel do
+    sha256 "201e4d90a029a2182fb70dc51b8474463ba125f47b99a6cac06f18326bb72e46"
+
+    url "https://github.com/nevse/conva/releases/download/v#{version}/conva-darwin-amd64.tar.gz"
   end
 
   def install
