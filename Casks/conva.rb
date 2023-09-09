38ea902c6265c4903f66f300b26041333a2e8a49
runner <runner@Mac-1694264284509.local>
2023-09-09 13:12:03 +0000

    conva 1.1.1

--- Casks/conva.rb
+++ Casks/conva.rb
@@ -1,13 +1,13 @@
 cask "conva" do
-  version "1.1.0"
+  version "1.1.1"
 
   on_arm do
-    sha256 "141cb4a7c35f8adb5c4ff850a5aeb98ec97ddc86ccdaddef48d0c0697b092b3f"
+    sha256 "f728f3cedd60b1b5a3aed4f3b0fbee813bd369fc023eff32d53520da0fe7b130"
 
     url "https://github.com/nevse/conva/releases/download/v#{version}/conva-darwin-arm64.tar.gz"
   end
   on_intel do
-    sha256 "1c3e919d0d70e21b93a2590a6122f01d4f7752dfd81e2fa6c21c5ffb4bb52a8d"
+    sha256 "86177ac77ede13a590928ee88a4e6d87022c23adee7d85347bc1723e24733605"
 
     url "https://github.com/nevse/conva/releases/download/v#{version}/conva-darwin-amd64.tar.gz"
   end
